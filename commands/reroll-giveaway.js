442e623b8fec7b860452ebe1f44e96584b760525
Androz2091 <androz2091@gmail.com>
2020-03-05 17:22:29 +0100

    Update to Discord.js v12

--- commands/reroll-giveaway.js
+++ commands/reroll-giveaway.js
@@ -3,7 +3,7 @@ const ms = require('ms');
 exports.run = async (client, message, args) => {
 
     // If the member doesn't have enough permissions
-    if(!message.member.hasPermission('MANAGE_MESSAGES') && !message.member.roles.some((r) => r.name === "Giveaways")){
+    if(!message.member.hasPermission('MANAGE_MESSAGES') && !message.member.roles.cache.some((r) => r.name === "Giveaways")){
         return message.channel.send(':x: You need to have the manage messages permissions to reroll giveaways.');
     }
 
--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,3 @@
 module.exports = (client) => {
-    console.log(`Ready to serve in ${client.channels.size} channels on ${client.guilds.size} servers, for a total of ${client.users.size} users.`);
+    console.log(`Ready to serve in ${client.channels.cache.size} channels on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users.`);
 };
