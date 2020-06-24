5708764c6ff8439edc1060f90682337ea07441dc
Androz2091 <androz2091@gmail.com>
2020-06-24 09:39:06 +0200

    :children_crossing: Remove exemptPermissions

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ client.giveawaysManager = new GiveawaysManager(client, {
     updateCountdownEvery: 5000,
     default: {
         botsCanWin: false,
-        exemptPermissions: [ "MANAGE_MESSAGES", "ADMINISTRATOR" ],
         embedColor: "#FF0000",
         reaction: "🎉"
     }
