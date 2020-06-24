9f83be328f7414f51c22312d2db6f4daf419517e
Androz2091 <androz2091@gmail.com>
2020-06-24 09:43:51 +0200

    :sound: Log client username in ready event

--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,3 @@
 module.exports = (client) => {
-    console.log(`Ready to serve in ${client.channels.cache.size} channels on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users.`);
+    console.log(`Ready as ${client.user.tag} to serve in ${client.channels.cache.size} channels on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users.`);
 };
