5536a7cdeb74764cd7cbd2d26e58dd712842d354
Androz2091 <androz2091@gmail.com>
2021-08-30 00:14:09 +0200

    :sparkles: Add pause and unpause commands

---
+++ commands/pause-giveaway.js
@@ -0,0 +1,65 @@
+const ms = require('ms');
+
+module.exports = {
+
+    description: 'Pause a giveaway',
+
+    options: [
+        {
+            name: 'giveaway',
+            description: 'The giveaway to pause (message ID or giveaway prize)',
+            type: 'STRING',
+            required: true
+        }
+    ],
+
+    run: async (client, interaction) => {
+
+        // If the member doesn't have enough permissions
+        if(!interaction.member.permissions.has('MANAGE_MESSAGES') && !interaction.member.roles.cache.some((r) => r.name === "Giveaways")){
+            return interaction.reply({
+                content: ':x: You need to have the manage messages permissions to pause giveaways.',
+                ephemeral: true
+            });
+        }
+
+        const query = interaction.options.getString('giveaway');
+
+        // try to found the giveaway with prize then with ID
+        const giveaway = 
+            // Search with giveaway prize
+            client.giveawaysManager.giveaways.find((g) => g.prize === query && g.guildId === interaction.guild.id) ||
+            // Search with giveaway ID
+            client.giveawaysManager.giveaways.find((g) => g.messageId === query && g.guildId === interaction.guild.id);
+
+        // If no giveaway was found
+        if (!giveaway) {
+            return interaction.reply({
+                content: 'Unable to find a giveaway for `'+ query + '`.',
+                ephemeral: true
+            });
+        }
+
+        if (giveaway.pauseOptions.isPaused) {
+            return interaction.reply({
+                content: 'This giveaway is already paused.',
+                ephemeral: true
+            });
+        }
+
+        // Edit the giveaway
+        client.giveawaysManager.pause(giveaway.messageId)
+        // Success message
+        .then(() => {
+            // Success message
+            interaction.reply('Giveaway paused!');
+        })
+        .catch((e) => {
+            interaction.reply({
+                content: e,
+                ephemeral: true
+            });
+        });
+
+    }
+};
---
+++ commands/unpause-giveaway.js
@@ -0,0 +1,65 @@
+const ms = require('ms');
+
+module.exports = {
+
+    description: 'Unpause a giveaway',
+
+    options: [
+        {
+            name: 'giveaway',
+            description: 'The giveaway to unpause (message ID or giveaway prize)',
+            type: 'STRING',
+            required: true
+        }
+    ],
+
+    run: async (client, interaction) => {
+
+        // If the member doesn't have enough permissions
+        if(!interaction.member.permissions.has('MANAGE_MESSAGES') && !interaction.member.roles.cache.some((r) => r.name === "Giveaways")){
+            return interaction.reply({
+                content: ':x: You need to have the manage messages permissions to unpause giveaways.',
+                ephemeral: true
+            });
+        }
+
+        const query = interaction.options.getString('giveaway');
+
+        // try to found the giveaway with prize then with ID
+        const giveaway = 
+            // Search with giveaway prize
+            client.giveawaysManager.giveaways.find((g) => g.prize === query && g.guildId === interaction.guild.id) ||
+            // Search with giveaway ID
+            client.giveawaysManager.giveaways.find((g) => g.messageId === query && g.guildId === interaction.guild.id);
+
+        // If no giveaway was found
+        if (!giveaway) {
+            return interaction.reply({
+                content: 'Unable to find a giveaway for `'+ query + '`.',
+                ephemeral: true
+            });
+        }
+
+        if (!giveaway.pauseOptions.isPaused) {
+            return interaction.reply({
+                content: 'This giveaway is not paused.',
+                ephemeral: true
+            });
+        }
+
+        // Edit the giveaway
+        client.giveawaysManager.unpause(giveaway.messageId)
+        // Success message
+        .then(() => {
+            // Success message
+            interaction.reply('Giveaway unpaused!');
+        })
+        .catch((e) => {
+            interaction.reply({
+                content: e,
+                ephemeral: true
+            });
+        });
+
+    }
+};
