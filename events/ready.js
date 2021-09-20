bd5a2a0d82c9b0d5e8fa285721c4767325299880
Aarav‏‏‎ ‎Mehta <aaravmehta59@outlook.com>
2021-09-20 19:21:47 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = (client) => {
     console.log(`Ready as ${client.user.tag} to serve in ${client.channels.cache.size} channels on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users.`);
-    const activities = [`All Giveaways Running Smoothly`, `Made By Androz2091`, `https://github.com/Androz2091/giveaways-bot/`];
+    const activities = [`All Giveaways Running Smoothly`, `Many Giveaways`, `People winning a prize`];
   setInterval(() => {
     let activity = activities[Math.floor(Math.random() * activities.length)];
     client.user.setActivity(activity, { type: "WATCHING" });
