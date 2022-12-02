5a3143a2861167834443bd86072a8af6243a68d6
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2022-12-02 22:53:07 +0500

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,14 @@
+const { ActivityType } = require("discord.js");
+
 module.exports = (client) => {
     console.log(`Ready as ${client.user.tag} to serve in ${client.channels.cache.size} channels on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users.`);
+    
+    const activities = [
+        `Giveaways in ${client.guilds.cache.size} guilds`,
+    ];
+    
+    setInterval(() => {
+        let activity = activities[Math.floor(Math.random() * activities.length)];
+        client.user.setActivity(activity, { type: ActivityType.Watching });
+    }, 20000);
 };
